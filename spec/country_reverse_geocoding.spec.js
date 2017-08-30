00e6abab17c9f252985735460d71e9a2bec1a437
Tariq Zabian <tzabian@gmail.com>
2017-08-30 11:25:04 +0400

    added ISO Alpha 2 Country Codes to each country property

--- spec/country_reverse_geocoding.spec.js
+++ spec/country_reverse_geocoding.spec.js
@@ -14,13 +14,16 @@ describe("Country reverse geocoding", function() {
     var results = crg.get_country(47.3, 0.7);
 
     expect(typeof results).toBe('object');
+    expect(typeof results.id).not.toBe('undefined');
     expect(typeof results.code).not.toBe('undefined');
     expect(typeof results.name).not.toBe('undefined');
     // This particular point should be in France
-    expect(results.code).toBe('FRA');
+    expect(results.id).toBe('FRA');
+    expect(results.code).toBe('FR');
 
     results = crg.get_country(1.352083 , 103.819836);
-    expect(results.code).toBe('SGP');
+    expect(results.id).toBe('SGP');
+    expect(results.code).toBe('SG')
   });
 
   it("should return null if no country was matched", function() {
@@ -32,10 +35,10 @@ describe("Country reverse geocoding", function() {
     var results = crg.get_us_state(32.377447, -86.300942);
 
     expect(typeof results).toBe('object');
-    expect(typeof results.code).not.toBe('undefined');
+    expect(typeof results.id).not.toBe('undefined');
     expect(typeof results.name).not.toBe('undefined');
     // This particular point should be in Alabama
-    expect(results.code).toBe('USA-AL');
+    expect(results.id).toBe('USA-AL');
   });
 
   it("should return null if no state was matched", function() {
@@ -44,19 +47,19 @@ describe("Country reverse geocoding", function() {
   });
 
   it("should return correct us state results", function() {
-    expect(crg.get_us_state(32.377447, -86.300942).code).toBe('USA-AL');
-    expect(crg.get_us_state(58.302197, -134.410467).code).toBe('USA-AK');
-    expect(crg.get_us_state(33.448097, -112.097094).code).toBe('USA-AZ');
-    expect(crg.get_us_state(34.746758, -92.288761).code).toBe('USA-AR');
-    expect(crg.get_us_state(38.576572, -121.493411).code).toBe('USA-CA');
-    expect(crg.get_us_state(38.186778, -84.875333).code).toBe('USA-KY');
-    expect(crg.get_us_state(30.457072, -91.187406).code).toBe('USA-LA');
-    expect(crg.get_us_state(44.307236, -69.781678).code).toBe('USA-ME');
-    expect(crg.get_us_state(35.682281, -105.939658).code).toBe('USA-NM');
-    expect(crg.get_us_state(35.780278, -78.639167).code).toBe('USA-NC');
-    expect(crg.get_us_state(46.820814, -100.782742).code).toBe('USA-ND');
-    expect(crg.get_us_state(44.938731, -123.030097).code).toBe('USA-OR');
-    expect(crg.get_us_state(41.830833, -71.415).code).toBe('USA-RI');
+    expect(crg.get_us_state(32.377447, -86.300942).id).toBe('USA-AL');
+    expect(crg.get_us_state(58.302197, -134.410467).id).toBe('USA-AK');
+    expect(crg.get_us_state(33.448097, -112.097094).id).toBe('USA-AZ');
+    expect(crg.get_us_state(34.746758, -92.288761).id).toBe('USA-AR');
+    expect(crg.get_us_state(38.576572, -121.493411).id).toBe('USA-CA');
+    expect(crg.get_us_state(38.186778, -84.875333).id).toBe('USA-KY');
+    expect(crg.get_us_state(30.457072, -91.187406).id).toBe('USA-LA');
+    expect(crg.get_us_state(44.307236, -69.781678).id).toBe('USA-ME');
+    expect(crg.get_us_state(35.682281, -105.939658).id).toBe('USA-NM');
+    expect(crg.get_us_state(35.780278, -78.639167).id).toBe('USA-NC');
+    expect(crg.get_us_state(46.820814, -100.782742).id).toBe('USA-ND');
+    expect(crg.get_us_state(44.938731, -123.030097).id).toBe('USA-OR');
+    expect(crg.get_us_state(41.830833, -71.415).id).toBe('USA-RI');
   });
 
 });
